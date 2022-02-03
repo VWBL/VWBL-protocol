fe0d8c147ad02866f7ef88433c9f029193d0cb14
seki <schokolade0317@gmail.com>
2022-02-03 17:12:40 +0900

    delete vwbl

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,14 +1,10 @@
-const VWBL = artifacts.require("VWBL");
 const lazyVWBL = artifacts.require("VWBLLazyMinting");
 const auction = artifacts.require("Auction");
 const market = artifacts.require("Market");
 const configs = require("./config");
 module.exports = async function (deployer, network, accounts) {
   const config = configs[network];
-  await deployer.deploy(VWBL, config.vwblMetadataUrl);
   await deployer.deploy(lazyVWBL, accounts[0], config.lazyMetadataUrl);
-  await deployer.deploy(auction, VWBL.address);
-  await deployer.deploy(market, VWBL.address);
   await deployer.deploy(auction, lazyVWBL.address);
   await deployer.deploy(market, lazyVWBL.address);
 };
