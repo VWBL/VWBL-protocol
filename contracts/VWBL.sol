ef6c81e0082adf6fe6663b22b872519c0a0d3341
seki <schokolade0317@gmail.com>
2022-02-12 16:29:08 +0900

    fix bug

--- contracts/VWBL.sol
+++ contracts/VWBL.sol
@@ -45,10 +45,11 @@ abstract contract VWBLProtocol is ERC721Enumerable, IERC2981 {
         view
         returns (TokenInfo[] memory)
     {
+        uint256 currentCounter = 0;
         TokenInfo[] memory tokens = new TokenInfo[](counter);
         for (uint256 i = 1; i <= counter; i++) {
             if (tokenIdToTokenInfo[i].minterAddress == minter) {
-                tokens.push(tokenIdToTokenInfo[i]);
+                tokens[currentCounter++] = tokenIdToTokenInfo[i];
             }
         }
         return tokens;
