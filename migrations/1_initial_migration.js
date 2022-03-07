526d208e7a8c64ec852404537b0d66802f72d3ff
Taki <hashimoto19980924@gmail.com>
2022-03-07 11:02:51 +0900

    minor fix

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,9 +1,7 @@
 const lazyVWBL = artifacts.require("VWBLLazyMinting");
-const tranferVWBLNFT = artifacts.require("TransferVWBLNFT");
 const configs = require("./config");
 module.exports = async function (deployer, network, accounts) {
   const config = configs[network];
   await deployer.deploy(lazyVWBL, accounts[0], config.lazyMetadataUrl);
   const lazyVWBLContract = await lazyVWBL.deployed();
-  await deployer.deploy(tranferVWBLNFT, lazyVWBLContract.address);
 };
