9a46d2d6ada47870a564c9e2674551ea3e88ab4f
seki <schokolade0317@gmail.com>
2022-03-31 16:47:59 +0900

    enable deploy

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,10 +1,13 @@
 const lazyVWBL = artifacts.require("VWBLLazyMinting");
 const vwblERC1155 = artifacts.require("VWBLERC1155");
+const vwblGateway = artifacts.require("VWBLGateway");
 
 const configs = require("./config");
 module.exports = async function (deployer, network, accounts) {
   const config = configs[network];
-  await deployer.deploy(lazyVWBL, accounts[0], config.lazyMetadataUrl, config.vwblGatewayAddress);
+  await deployer.deploy(vwblGateway);
+  const vwblGatewayContract = await vwblGateway.deployed();
+  await deployer.deploy(lazyVWBL, accounts[0], config.lazyMetadataUrl, vwblGatewayContract.address);
   const lazyVWBLContract = await lazyVWBL.deployed();
   await deployer.deploy(vwblERC1155, config.vwblMetadataUrl);
   const vwblERC1155Contract = await vwblERC1155.deployed();
