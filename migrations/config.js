28e1184c1ebaaa221bbc42aa5be55cfe936b8ba1
seki <schokolade0317@gmail.com>
2022-04-01 19:08:05 +0900

    enable to deploy to ropsten

--- migrations/config.js
+++ migrations/config.js
@@ -6,5 +6,9 @@ module.exports={
   polygon: {
     vwblMetadataUrl: "http://xxx.yyy.com",
     lazyMetadataUrl: "http://xxx.zzz.com",
+  },
+  ropsten: {
+    vwblMetadataUrl: "http://xxx.yyy.com",
+    lazyMetadataUrl: "http://xxx.zzz.com",
   }
 };
