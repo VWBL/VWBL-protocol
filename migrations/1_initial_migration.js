21239a35f7f7f96308d558288e8850f6075189dc
Shunsuke Nagashima <42495880+ShunsukeNagashima@users.noreply.github.com>
2022-04-06 17:59:36 +0900

    chore: fix migration

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,16 +1,7 @@
-const lazyVWBL = artifacts.require("VWBLLazyMinting")
-const vwblERC1155 = artifacts.require("VWBLERC1155")
-const vwblERC721 = artifacts.require("VWBL")
-const ExternalNFT = artifacts.require("ExternalNFT")
-const vwblGateway = artifacts.require("VWBLGateway")
+const vwbl = artifacts.require("VWBL")
 
 const configs = require("./config")
-module.exports = async function (deployer, network, accounts) {
+module.exports = async function (deployer, network) {
   const config = configs[network]
-  await deployer.deploy(vwblGateway, "1000000000000000000")
-  const vwblGatewayContract = await vwblGateway.deployed()
-  await deployer.deploy(lazyVWBL, accounts[0], config.lazyMetadataUrl, vwblGatewayContract.address)
-  await deployer.deploy(vwblERC721, config.lazyMetadataUrl, vwblGatewayContract.address)
-  await deployer.deploy(vwblERC1155, config.vwblMetadataUrl)
-  await deployer.deploy(ExternalNFT)
+  await deployer.deploy(vwbl, config.vwblMetadataUrl, config.gatewayContractAddress)
 }
--- migrations/config.js
+++ migrations/config.js
@@ -1,14 +1,10 @@
 module.exports={
   develop: {
     vwblMetadataUrl: "http://xxx.yyy.com",
-    lazyMetadataUrl: "http://xxx.zzz.com",
+    gatewayContractAddress: "0x0000000000000000000000000000000000000000"
   },
   polygon: {
     vwblMetadataUrl: "http://xxx.yyy.com",
-    lazyMetadataUrl: "http://xxx.zzz.com",
+    gatewayContractAddress: "0x49098dd8c74168144979738943076a0991574da8"
   },
-  ropsten: {
-    vwblMetadataUrl: "http://xxx.yyy.com",
-    lazyMetadataUrl: "http://xxx.zzz.com",
-  }
 };
