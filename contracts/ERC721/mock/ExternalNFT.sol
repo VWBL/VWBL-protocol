cd5a24adc708d34af7ab61e3335c92b51130f9cc
Shunsuke Nagashima <42495880+ShunsukeNagashima@users.noreply.github.com>
2022-04-06 17:49:36 +0900

    delete unused files

--- contracts/ERC721/mock/ExternalNFT.sol
+++
@@ -1,7 +0,0 @@
-//SPDX-License-Identifier: ISC
-pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
-
-contract ExternalNFT is ERC721PresetMinterPauserAutoId {
-    constructor() ERC721PresetMinterPauserAutoId("VWBL NFT", "VWBL", "http://xxx.zzz.com") {}
-}
