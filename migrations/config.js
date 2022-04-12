6fd8685fde47070de14141d5f0ffb71f1d42be74
Shunsuke Nagashima <42495880+ShunsukeNagashima@users.noreply.github.com>
2022-04-12 13:21:32 +0900

    chore: add config for rinkeby

--- migrations/config.js
+++ migrations/config.js
@@ -7,4 +7,8 @@ module.exports={
     vwblMetadataUrl: "http://xxx.yyy.com",
     gatewayContractAddress: "0x49098dd8c74168144979738943076a0991574da8"
   },
+  rinkeby: {
+    vwblMetadataUrl: "http://xxx.yyy.com",
+    gatewayContractAddress: "0x54df5c4654598bB3D849beE3684309E66EB5905F"
+  }
 };
--- truffle-config.js
+++ truffle-config.js
@@ -75,6 +75,14 @@ module.exports = {
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/5f937c05d61b42aa91b88e48281b9cb9`),
+      network_id: 4,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
