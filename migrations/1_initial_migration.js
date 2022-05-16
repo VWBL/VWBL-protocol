3547e1fe85c609ff1048850fa8582b5acf4a2ff5
Asuma Yamada <yamadaasuma@gmail.com>
2022-05-16 19:24:42 +0900

    fix: config

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -7,10 +7,17 @@ const vwblGateway = artifacts.require("VWBLGateway")
 const configs = require("./config")
 module.exports = async function (deployer, network, accounts) {
   const config = configs[network]
-  await deployer.deploy(vwblGateway, "1000000000000000000")
-  const vwblGatewayContract = await vwblGateway.deployed()
-  await deployer.deploy(lazyVWBL, accounts[0], config.lazyMetadataUrl, vwblGatewayContract.address)
-  await deployer.deploy(vwblERC721, config.lazyMetadataUrl, vwblGatewayContract.address)
-  await deployer.deploy(vwblERC1155, config.vwblMetadataUrl)
-  await deployer.deploy(ExternalNFT)
+  if(network !== 'develop') {
+    await deployer.deploy(vwblGateway, "1000000000000000000")
+    const vwblGatewayContract = await vwblGateway.deployed()
+    await deployer.deploy(vwblERC721, config.lazyMetadataUrl, vwblGatewayContract.address)
+  } else {
+    // when testing
+    await deployer.deploy(vwblGateway, "1000000000000000000")
+    const vwblGatewayContract = await vwblGateway.deployed()
+    await deployer.deploy(lazyVWBL, accounts[0], config.lazyMetadataUrl, vwblGatewayContract.address)
+    await deployer.deploy(vwblERC721, config.lazyMetadataUrl, vwblGatewayContract.address)
+    await deployer.deploy(vwblERC1155, config.vwblMetadataUrl)
+    await deployer.deploy(ExternalNFT)
+  }
 }
