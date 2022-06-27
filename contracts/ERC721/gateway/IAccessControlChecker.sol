3eece344cf430ec375c76a4e4ec9ce810d098599
Taki <hashimoto19980924@gmail.com>
2022-06-27 20:18:57 +0900

    rm comment

--- contracts/ERC721/gateway/IAccessControlChecker.sol
+++ contracts/ERC721/gateway/IAccessControlChecker.sol
@@ -1,10 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-/**
- * @dev Interface of the VWBL Access Control Checker as defined in the
- * https://github.com/VWBL-protocol/contracts/ERC721/gateway/AcessControlChecker.sol
- */
 interface IAccessControlChecker {
     function checkAccessControl(address user, bytes32 documentId) external view returns (bool);
 }
