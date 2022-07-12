697211106dc69c350ff40bf7299965c33cd9dc18
Tatsuki <40402518+thashimoto1998@users.noreply.github.com>
2022-07-12 17:14:09 +0900

    Revert "fix access condition path"

--- contracts/ERC721/VWBL.sol
+++ contracts/ERC721/VWBL.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/interfaces/IERC2981.sol";
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
 import "./IVWBL.sol";
-import "../access-condition/IAccessControlCheckerByNFT.sol";
+import "./gateway/IAccessControlCheckerByNFT.sol";
 import "./gateway/IVWBLGateway.sol";
 
 abstract contract VWBLProtocol is ERC721Enumerable, IERC2981 {
