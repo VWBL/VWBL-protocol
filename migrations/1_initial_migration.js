51cfb6d15caa64299a0a97f7e5d7ad344e4dc871
Taki <hashimoto19980924@gmail.com>
2022-10-12 12:49:49 +0900

    fix deploy script

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,28 +2,34 @@ const vwblERC721 = artifacts.require("VWBL")
 const vwblMetadata = artifacts.require("VWBLMetadata")
 const ExternalNFT = artifacts.require("ExternalNFT")
 const vwblGateway = artifacts.require("VWBLGateway")
+const gatewayProxy = artifacts.require("GatewayProxy");
 const accessControlCheckerByNFT = artifacts.require("AccessControlCheckerByNFT")
-
 const configs = require("./config")
 
 const migrateTest = async (config, deployer) => {
   await deployer.deploy(vwblGateway, "1000000000000000000")
   const vwblGatewayContract = await vwblGateway.deployed()
-  await deployer.deploy(accessControlCheckerByNFT, vwblGatewayContract.address);
+  await deployer.deploy(gatewayProxy, vwblGatewayContract.address);
+  const gatewayProxyContract = await gatewayProxy.deployed();
+  await deployer.deploy(accessControlCheckerByNFT, gatewayProxyContract.address);
   const accessControlCheckerByNFTContract = await accessControlCheckerByNFT.deployed();
-  await deployer.deploy(vwblERC721, config.vwblMetadataUrl, vwblGatewayContract.address, accessControlCheckerByNFTContract.address)
+  await deployer.deploy(vwblERC721, config.vwblMetadataUrl, gatewayProxyContract.address, accessControlCheckerByNFTContract.address)
   await deployer.deploy(ExternalNFT)
-  await deployer.deploy(vwblMetadata, vwblGatewayContract.address, accessControlCheckerByNFTContract.address)
+  await deployer.deploy(vwblMetadata, gatewayProxyContract.address, accessControlCheckerByNFTContract.address)
 }
 
 const migrateERC721 = async (config, deployer) => {
   console.log('VWBL Metadata URL: ', config.vwblMetadataUrl)
-  await deployer.deploy(vwblGateway, "10000000000000000") // 0.01 ETH
+  await deployer.deploy(vwblGateway, "0") 
   const vwblGatewayContract = await vwblGateway.deployed()
-  await deployer.deploy(accessControlCheckerByNFT, vwblGatewayContract.address);
+  await deployer.deploy(gatewayProxy, vwblGatewayContract.address);
+  const gatewayProxyContract = await gatewayProxy.deployed();
+  await deployer.deploy(accessControlCheckerByNFT, gatewayProxyContract.address);
   const accessControlCheckerByNFTContract = await accessControlCheckerByNFT.deployed();
-  await deployer.deploy(vwblERC721, config.vwblMetadataUrl, vwblGatewayContract.address, accessControlCheckerByNFTContract.address)
-  await deployer.deploy(vwblMetadata, vwblGatewayContract.address, accessControlCheckerByNFTContract.address)
+  await deployer.deploy(vwblERC721, config.vwblMetadataUrl, gatewayProxyContract.address, accessControlCheckerByNFTContract.address);
+  await vwblERC721.deployed();
+  await deployer.deploy(vwblMetadata, gatewayProxyContract.address, accessControlCheckerByNFTContract.address)
+  await vwblMetadata.deployed();
 }
 
 module.exports = async function (deployer, network) {
