4863dc782f0b1dee6cd34cebab7adc785279d8f7
Tatsuki <40402518+thashimoto1998@users.noreply.github.com>
2022-10-19 15:51:39 +0900

    Update config.js

--- migrations/config.js
+++ migrations/config.js
@@ -10,11 +10,10 @@ module.exports={
   },
   polygon: {
     vwblMetadataUrl: process.env.VWBL_METADATA_URL || "http://xxx.yyy.com",
+    gatewayProxyContractAddress: "0xa0cbAF6872f80172Bf0a471bC447440edFEC4475",
+    accessControlCheckerByNFTContractAddress: "0x9c9bd1b3376ccf3d695d9233c04e865e556f8980",
   },
-  ropsten: {
-    vwblMetadataUrl: process.env.VWBL_METADATA_URL || "http://xxx.yyy.com",
-  },
-  rinkeby: {
+  goerli: {
     vwblMetadataUrl: process.env.VWBL_METADATA_URL || "http://xxx.yyy.com",
   }
 };
