4f899a2e7839f10aeddf147d4658932d5d8f9090
Taki <hashimoto19980924@gmail.com>
2024-04-25 20:17:20 +0900

    chore: mkdir vwbl-contract-wallet

--- contracts/vwbl-contract-wallet/AllocateVWBLFee.sol
+++ contracts/vwbl-contract-wallet/AllocateVWBLFee.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./ValidatorRegistry.sol";
-import "../IVWBLGatewayV2.sol";
-import "../legacy/VWBLGateway.sol";
-import "./IStableCoinFeeRegistry.sol";
+import "../gateway/IVWBLGatewayV2.sol";
+import "../gateway/legacy/VWBLGateway.sol";
+import "../gateway/utils/IStableCoinFeeRegistry.sol";
 
 contract AllocateVWBLFee is ValidatorRegistry, ReentrancyGuard {
     address public gatewayV1Address;
