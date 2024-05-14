49e1875551ad420b570da6ff370eadcb963b8f79
Tatsuki <40402518+thashimoto1998@users.noreply.github.com>
2024-05-14 19:52:50 +0900

    Merge pull request #88 from VWBL/feat/grant
    feat: grant view permission

--- contracts/access-condition/ERC721/AccessControlCheckerByNFT.sol
+++ contracts/access-condition/ERC721/AccessControlCheckerByNFT.sol
@@ -9,9 +9,10 @@ import "../IAccessControlChecker.sol";
 import "./IAccessControlCheckerByNFT.sol";
 import "../AbstractControlChecker.sol";
 import "../IVWBL.sol";
+import "./IViewPermission.sol";
 
 /**
- * @dev VWBL's access condition contract which is defined by NFT Owner has access right of digital content
+ * @dev VWBL's access condition contract which is defined by NFT Owner and Grantee has access right of digital content
  *      and NFT Minter is digital contract creator(decryption key creator)
  */
 contract AccessControlCheckerByNFT is AbstractControlChecker, Ownable {
@@ -74,8 +75,13 @@ contract AccessControlCheckerByNFT is AbstractControlChecker, Ownable {
      * @param user The address of decryption key requester or decryption key sender to VWBL Network
      * @param documentId The Identifier of digital content and decryption key
      */
-    function checkAccessControl(address user, bytes32 documentId) external pure returns (bool) {
-        return false;
+    function checkAccessControl(address user, bytes32 documentId) external view returns (bool) {
+        Token memory token = documentIdToToken[documentId];
+        try IViewPermission(token.contractAddress).checkViewPermission(token.tokenId, user) returns (bool result) {
+            return result;
+        } catch {
+            return false;
+        }
     }
 
     /**
