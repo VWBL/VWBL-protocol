181d10e447986c0e77a460e881f73eecfd2ab449
Huzitatuguharu <sgrko.4751171@gmail.com>
2024-07-11 14:34:07 +0900

    registries配列の構造変更、正常系テスト修正

--- test/vwblcontractwallet.test.ts
+++ test/vwblcontractwallet.test.ts
@@ -1,35 +1,43 @@
 import { expect } from "chai"
-import hre, { ethers } from "hardhat"
+import { ethers } from "hardhat"
 
 import { VWBLContractWallet } from "../typechain-types"
 import { deployContractsGatewayV2 } from "./lib/deployContracts"
+import { ZeroAddress } from "ethers"
 
 describe("VWBLContractWallet", function () {
+    interface Registries {
+        stableCoinFeeRegistry: any
+        withdrawExtraFeeRegistry: any
+        vwblGatewayV1Registry: any
+        vwblGatewayV2Registry: any
+    }
     let deploymentInfo: any
     let vwblContractWallet: VWBLContractWallet
     let accounts: any[]
-    let registries: any[]
-
+    let registries: Registries
     const newFeeNumerator = 1500
     const newFeeWei = ethers.parseEther("0.001")
+
     before(async function () {
         deploymentInfo = await deployContractsGatewayV2()
         vwblContractWallet = deploymentInfo.vwblContractWallet
         accounts = deploymentInfo.accounts
-        registries = [
-            { registry: deploymentInfo.stableCoinFeeRegistry, name: "stableCoinFeeRegistry" },
-            { registry: deploymentInfo.withdrawExtraFeeRegistry, name: "withdrawExtraFeeRegistry" },
-            { registry: deploymentInfo.vwblGatewayV1Registry, name: "vwblGatewayV1Registry" },
-            { registry: deploymentInfo.vwblGatewayV2Registry, name: "vwblGatewayV2Registry" },
-        ]
+
+        registries = {
+            stableCoinFeeRegistry: deploymentInfo.stableCoinFeeRegistry,
+            withdrawExtraFeeRegistry: deploymentInfo.withdrawExtraFeeRegistry,
+            vwblGatewayV1Registry: deploymentInfo.vwblGatewayV1Registry,
+            vwblGatewayV2Registry: deploymentInfo.vwblGatewayV2Registry,
+        }
     })
 
     describe("Ownership and Access Control Tests", function () {
         it("should handle ownership transfers and contract registration correctly for all registries", async function () {
-            for (const { registry, name } of registries) {
+            // 各レジストリのテスト
+            for (const [name, registry] of Object.entries(registries)) {
                 console.log(`Testing ${name}`)
 
-                // Test transfer ownership
                 // 所有権移譲のテスト: 非所有者による移譲を試み、失敗を確認
                 await expect(
                     registry.connect(accounts[1]).transferOwnership(accounts[1].address)
@@ -39,6 +47,7 @@ describe("VWBLContractWallet", function () {
                 await registry.connect(accounts[0]).transferOwnership(await vwblContractWallet.getAddress())
                 expect(await registry.owner()).to.eq(await vwblContractWallet.getAddress())
 
+                // コントラクト登録のテスト
                 const initialOwnedContracts = await vwblContractWallet.getOwnedContracts()
                 await vwblContractWallet
                     .connect(accounts[0])
@@ -50,10 +59,12 @@ describe("VWBLContractWallet", function () {
                 expect(isRegistered).to.be.true
                 expect(finalOwnedContracts.length).to.equal(initialOwnedContracts.length + 1)
             }
+            console.log("All tests for ownership and access control completed successfully.")
         })
     })
-    describe("Stable Coin Fee Registry Operations", function () {
-        const fiatIndex = 0 // Example index
+
+    describe("VWBLContractWallet - Negative Cases", function () {
+        const fiatIndex = 1
         const fiatName = "USD"
         const newFiatName = "EUR"
         const erc20Addresses = [ethers.Wallet.createRandom().address]
@@ -129,82 +140,80 @@ describe("VWBLContractWallet", function () {
                 vwblContractWallet.connect(accounts[1]).setStableCoinFeeRegistry(newScFeeRegistryAddress)
             ).to.be.revertedWith("msg sender doesn't have OPERATOR_ROLE")
         })
+        // withdrawAllPendingToken
+        it("should fail to withdraw all pending tokens to a zero address", async function () {
+            // ゼロアドレスへの全トークンの引き出しを試みる
+            await expect(vwblContractWallet.withdrawAllPendingToken(ZeroAddress)).to.be.revertedWith(
+                "Invalid recipient address"
+            )
+        })
     })
 
     describe("VWBLContractWallet - Positive Cases", function () {
-        let fiatIndex: any
-        let fiatName: any
-        let newFiatName: any
-        let erc20Addresses: any
-        let newERC20Addresses: any
-        let decimalses: any
-        let feeNumerator: any
-        beforeEach(async function () {
-            fiatIndex = 0
-            fiatName = "USD"
-            newFiatName = "EUR"
-            erc20Addresses = [ethers.Wallet.createRandom().address]
-            newERC20Addresses = [ethers.Wallet.createRandom().address]
-            decimalses = [18]
-            feeNumerator = 1000
-            const registeredTokens = await registries[0].registry.getRegisteredTokens()
-            console.log("registeredTokens:>>>>>>>", registeredTokens)
-            console.log("erc20Addresses:>>>>>>>", erc20Addresses)
-        })
+        const fiatIndex = 1
+        const fiatName = "USD"
+        const newFiatName = "EUR"
+        const erc20Addresses = [ethers.Wallet.createRandom().address]
+        const newERC20Addresses = [ethers.Wallet.createRandom().address]
+        const decimalses = [18]
+        const feeNumerator = 1000
 
         // setNativeTokenFee
         it("should set the native token fee successfully", async function () {
+            // 初期値を取得
+            const initialFeeWeiV1 = await registries.vwblGatewayV1Registry.feeWei()
+            const initialFeeWeiV2 = await registries.vwblGatewayV2Registry.feeWei()
             await vwblContractWallet.setNativeTokenFee(newFeeWei)
+            // 新しい手数料を取得
+            const updatedFeeWeiV1 = await registries.vwblGatewayV1Registry.feeWei()
+            const updatedFeeWeiV2 = await registries.vwblGatewayV2Registry.feeWei()
+            // 初期値と更新後の値を比較
+            expect(updatedFeeWeiV1).to.not.equal(initialFeeWeiV1)
+            expect(updatedFeeWeiV1).to.equal(newFeeWei)
+            expect(updatedFeeWeiV2).to.not.equal(initialFeeWeiV2)
+            expect(updatedFeeWeiV2).to.equal(newFeeWei)
         })
 
         // setStableCoinFee
         it("should set a stable coin fee successfully", async function () {
-            await expect(vwblContractWallet.setStableCoinFee(fiatIndex, newFeeNumerator)).to.not.be.reverted
+            await expect(vwblContractWallet.setStableCoinFee(0, newFeeNumerator)).to.not.be.reverted
         })
         // registerStableCoinInfo
         it("should register stable coin info successfully", async function () {
             await expect(vwblContractWallet.registerStableCoinInfo(fiatName, erc20Addresses, decimalses, feeNumerator))
                 .to.not.be.reverted
         })
-        // renameFiat
         it("should rename fiat currency successfully", async function () {
-            await vwblContractWallet.registerStableCoinInfo(fiatName, erc20Addresses, decimalses, feeNumerator)
-            await vwblContractWallet.renameFiat(fiatIndex, newFiatName)
+            await vwblContractWallet.renameFiat(1, newFiatName)
+            const postRenameInfo = await registries.stableCoinFeeRegistry.getStableCoinInfos()
+            expect(postRenameInfo[0][0]).to.equal(newFiatName)
         })
 
         // registerERC20Addresses
         it("should register new ERC20 addresses successfully", async function () {
-            for (const address of newERC20Addresses) {
-                expect(await registries[0].registry.registered(address)).to.be.false
-            }
-            await vwblContractWallet.registerERC20Addresses(fiatIndex, erc20Addresses, decimalses)
+            await vwblContractWallet.registerERC20Addresses(fiatIndex, newERC20Addresses, decimalses)
         })
 
         // unregisterERC20Address
         it("should unregister an ERC20 address successfully", async function () {
-            await vwblContractWallet.registerERC20Addresses(fiatIndex, erc20Addresses, decimalses)
-            const registeredTokens = await registries[0].registry.getRegisteredTokens()
-            console.log("registeredTokens:>>>>>>>", registeredTokens)
-            await vwblContractWallet.unregisterERC20Address(1, "0xbb472b290f661295d0e8702e85c7de22026e9d1e")
+            const initialTokenInfo = await registries.stableCoinFeeRegistry.getStableCoinInfos()
+            const ERC20Address = initialTokenInfo[0][1][0]
+            await vwblContractWallet.unregisterERC20Address(1, ERC20Address)
         })
 
         // setRecipient
         it("should set recipient successfully", async function () {
             const srcAddress = ethers.Wallet.createRandom().address
             const recipient = ethers.Wallet.createRandom().address
-
             await expect(vwblContractWallet.setRecipient(srcAddress, recipient)).to.not.be.reverted
         })
 
         // setStableCoinFeeRegistry
         it("should set Stable Coin Fee Registry address successfully", async function () {
             const newScFeeRegistryAddress = ethers.Wallet.createRandom().address
-
             await expect(vwblContractWallet.setStableCoinFeeRegistry(newScFeeRegistryAddress))
                 .to.emit(vwblContractWallet, "stableCoinFeeRegistryChanged")
-                .withArgs(registries[0].registry, newScFeeRegistryAddress)
-
-            // アドレスが正しく更新されたか確認
+                .withArgs(registries.stableCoinFeeRegistry, newScFeeRegistryAddress)
             expect(await vwblContractWallet.scFeeRegistryAddress()).to.equal(newScFeeRegistryAddress)
         })
     })
