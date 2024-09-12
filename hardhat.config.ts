76bd81db7cfd569b1111608a95abbe4a0992c92d
Tatsuki <hashimoto19980924@gmail.com>
2024-09-12 20:23:02 +0900

    feat: update hardhat package

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,22 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "hardhat-gas-reporter";
 import { vars } from "hardhat/config";
 
 const PRIVATE_KEY = vars.get("PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.20",
+    version: "0.8.24",
     settings: {
       optimizer: {
           enabled: true,
           runs: 200,
       },
     },
-  },
-  gasReporter: {
-    enabled: true, 
   },
   networks: {
     hardhat: {
